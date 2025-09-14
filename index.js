122e36032036abb770b3db91f395ea0f0c671ef1
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2025-09-14 07:12:36 +0530

    added the function in js for the decrease, reset and save button

--- index.js
+++ index.js
@@ -7,7 +7,33 @@ let countBtn = document.getElementById("countBtn");
 
 let count = 0;
 
+// Increment function
+
 function increment() {
   count = count + 1;
   countBtn.textContent = count;
+}
+
+// Decrement function
+
+function decrement() {
+  count -= 1;
+  countBtn.textContent = count;
+}
+
+// Reset function
+
+function reset() {
+  count = 0;
+  countBtn.textContent = count;
+}
+
+// Save function
+
+let saveBtn = document.getElementById("save-btn");
+let entries = document.getElementById("entries");
+
+function save() {
+  let counterStr = count + ", ";
+  entries.textContent += counterStr;
 }
